ec591ac193dc01da4eaa95d3336ad95d39a659d9
22prerak22 <89489828+22prerak22@users.noreply.github.com>
2025-06-21 14:10:28 -0400

    Update Agent.tsx

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -75,13 +75,18 @@ const Agent = ({ userName, userId, type }: AgentProps) => {
 
   const handleCall = async () => {
     setCallStatus(CallStatus.CONNECTING);
-
-    await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
-      variableValues: {
-        user_name: userName,
-        user_id: userId,
-      },
-    });
+    await vapi.start(
+      undefined,
+      undefined,
+      undefined,
+      process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!,
+      {
+        variableValues: {
+          username: userName,
+          userid: userId,
+        },
+      }
+    );
   };
   const handleDisconnect = () => {
     setCallStatus(CallStatus.FINISHED);
